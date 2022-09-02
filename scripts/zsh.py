b5121d461e24dbca83321ab0970bdc741c535b15
Sean Breckenridge <seanbrecke@gmail.com>
2022-09-01 17:39:51 -0700

    zsh/bash: create unique strings instead of using repr

--- scripts/zsh.py
+++ scripts/zsh.py
@@ -9,7 +9,8 @@ from line_normalizer import LineNormalizer
 class Normalizer(LineNormalizer):
     @classmethod
     def parse_file(cls, path: Path) -> Iterator[Any]:
-        yield from _parse_file(path)
+        for e in _parse_file(path):
+            yield f"{e.dt.timestamp()} {e.duration} {e.command}"
 
 
 if __name__ == "__main__":
